011848931e738c9873a764a0b195a6288520c531
isaiaha03 <isaiahalvarez2003@gmail.com>
2024-02-15 23:55:32 -0600

    Finished the exercise

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+            Console.WriteLine("Try to guess my favorite number!");
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNumber) 
+            {
+                Console.WriteLine("Too low.");
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine("Too high.");
+            }
+            else
+            {
+                Console.WriteLine("Congrats! You guessed correctly.");
+            }
         }
     }
 }
